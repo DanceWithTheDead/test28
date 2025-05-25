1803b20299ac7923efaec16c77332db212ee852e
DWTD <zhora.mosa@mail.ru>
2025-05-25 14:05:44 +0300

    Create migrations

---
+++ database/migrations/2025_05_25_104547_create_cars_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Api\CarModel;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(CarModel::class)->constrained();
+            $table->integer('year')->nullable();
+            $table->integer('mileage')->nullable();
+            $table->string('color')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
