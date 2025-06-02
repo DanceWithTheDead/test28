7168a6b3d1a95b35a10f2488b925be36f42255b4
DWTD <zhora.mosa@mail.ru>
2025-06-02 10:33:19 +0300

    finally

--- app/Http/Controllers/Api/CarModelController.php
+++ app/Http/Controllers/Api/CarModelController.php
@@ -14,7 +14,7 @@ class CarModelController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(CarModel::all());
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,13 @@
 
 use App\Http\Controllers\Api\BrandController;
 use App\Http\Controllers\Api\CarController;
+use App\Http\Controllers\Api\CarModelController;
 use App\Models\CarModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/brands', [BrandController::class, 'index']);
+Route::get('/models', [CarModelController::class, 'index']);
 
 
 Route::post('/cars', [CarController::class, 'store']);
